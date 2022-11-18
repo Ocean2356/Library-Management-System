694df2b5e4796ab543b6ad12598b2d04123c1203
Abdallah Tafraoui <abdallah.tafraoui@etu.utc.fr>
2022-11-18 16:39:25 +0000

    Update gerer_user.py

--- rendu 4/gerer_user.py
+++ rendu 4/gerer_user.py
@@ -7,32 +7,32 @@ def gerer_user(cur, login):
     print("1 pour sanctionner un adhérant")
     print("2 pour blacklister un adhérant")
     print("3 pour mettre a jour la dette de l'adhérant")
-    user_choix1 = input("Selectionnez un choix")
-    if user_choix1 == 1:
-        user_choix2 = input("1 pour suspendre temporairement un adhérant")
-        if user_choix2 == 1:
+    choix1 = input("Selectionnez un choix : ")
+    if choix1 == "1":
+        choix2 = input("Selectionnez 1 pour suspendre temporairement un adhérant")
+        if choix2 == "1":
             dureesanction = input("Inserez la durée de la sanction ")
             sql = "insert into sanction values ('%s');" % (dureesanction)
             cur.execute(sql)
         else:
             exit()
-    elif user_choix1 == 2:
+    elif choix1 == "2":
         print("Etes vous sur de vouloir blacklister l'adherant ?")
-        user_choix3 = input("Selectionnez 1 pour Oui et 0 pour Non")
-        if user_choix3 == 1:
+        choix3 = input("Selectionnez 1 pour Oui et 0 pour Non")
+        if choix3 == "1":
             sql = "insert into sanction values (1);"
             cur.execute(sql)
         else:
             exit()
 
-    elif user_choix1 == 3:
-        sql = "select sommeDue from sanction;"
+    elif choix1 == "3":
+        sql = "select sommeDu from sanction;"
         cur.execute(sql)
         montant = cur.fetchall()
         print("La somme due est de ", montant)
-        user_choix4 = input(
+        choix4 = input(
             "Selectionnez 1 pour mettre à jour le montant de la somme de et 0 pour quitter")
-        if user_choix4 == 1:
+        if choix4 == "1":
             montantDette = input("Inserez la durée de la sanction ")
             sql = "insert into sanction values ('%s');" % (montantDette)
             cur.execute(sql)
