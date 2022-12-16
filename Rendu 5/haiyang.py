e285a4fa94af09e25ad91d3fdfc767075ad52ca3
Lilian Valin <lilian.valin@etu.utc.fr>
2022-12-16 17:00:33 +0000

    Update haiyang.py

--- Rendu 5/haiyang.py
+++ Rendu 5/haiyang.py
@@ -1,73 +1,58 @@
 def recherche(cur):
     print("Entrer les mots clés à rechercher pour chaque champ")
-    t = input("Titre : ")
+    titre = input("Titre : ")
     # d = input("Date d'apparition : ")
-    e = input("Éditeur : ")
-    g = input("Genre : ")
+    editeur = input("Éditeur : ")
+    genre = input("Genre : ")
     # c = input("Contributeur : ")
-    sql = """select r.*
-            FROM ressource r
-            WHERE r.titre LIKE '%"""+t+"""%'
-                AND r.editeur LIKE '%"""+e+"""%'
-                AND r.genre LIKE '%"""+g+"""%'
-                ;"""
+    sql = """SELECT r.* FROM ressource r WHERE r.titre LIKE '%"""+titre+"""%' AND r.editeur LIKE '%"""+editeur+"""%' AND r.genre LIKE '%"""+genre+"""%';"""
     cur.execute(sql)
     raw = cur.fetchall()
     for ligne in raw:
-        print(ligne)
+        print("--------------------------------------------------------")
+        print("Le titre du document est : ",ligne[1])
+        print("Il a était publié le : ",ligne[2])
+        print("Il a était publié par : ",ligne[3])
+        print("Le genre du document est : ",ligne[4])
+        print("Le code de classification de la ressource est : ",ligne[5])
+        print("Le code de la ressource est : ",ligne[0])
         code = ligne[0]
-        
-        sql = """select * 
-                from livre l 
-                where l.ressource = '%d'
-                ;""" % code
+
+        # Livre
+        sql = """SELECT * FROM livre l WHERE l.ressource = '%d';""" % code
         cur.execute(sql)
         spe = cur.fetchone()
         if spe is not None:
-            print(spe)
+            print("L'ISBN du lives est : ",spe[0])
+            print("La langue du livre est : ",spe[1])
+            print("Le résumé du livre est : \"",spe[2], "\"")
             code = spe[3]
-            sql = """select c.*
-                    from auteur p, contributeur c
-                    where p.livre = %d
-                        and p.id_contributeur = c.id_contributeur
-                    ;""" % code
+            sql = """select c.* from auteur p, contributeur c where p.livre = %d and p.id_contributeur = c.id_contributeur ;""" % code
             cur.execute(sql)
             contributeur = cur.fetchall()
             for auteur in contributeur:
-                print(auteur)
-            
+                print("L'auteur(e) du livre s'appelle",auteur[2],auteur[1])
+                print("Il/Elle est né le : ",auteur[3])
+                print("Il/Elle est : ",auteur[4])
+
+        # Film
         else:
-            sql = """select * 
-                    from film l 
-                    where l.ressource = '%d'
-                    ;""" % code
+            sql = """select * from film l where l.ressource = '%d';""" % code
             cur.execute(sql)
             spe = cur.fetchone()
             if spe is not None:
-                print(spe)
-                
+                print("La langue du film est : ", spe[0])
+                print("Le film dure : ", spe[1])
+                print("Le synopsis du film est : \"", spe[2], "\"")
+
+            #Musique
             else:
-                sql = """select * 
-                        from oeuvre_musicale l 
-                        where l.ressource = '%d'
-                        ;""" % code
+                sql = """select * from oeuvre_musicale l where l.ressource = '%d';""" % code
                 cur.execute(sql)
                 spe = cur.fetchone()
-        
-                print(spe)
+                print("La durée de la musique est :",spe[0])
         
         print()
-        
-"""def visualiser(cur, login):
-    sql = SELECT *
-            FROM pret
-            WHERE adherent = '%s'
-            ; % (login)
-    cur.execute(sql)
-    raw = cur.fetchall()
-    for ligne in raw:
-        print(ligne)
-"""
 
 def visualiser(cur, login):
     user_choix1 = -1
